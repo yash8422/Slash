49b2000ed59f705be3d7295f3f0d2e71c6245564
yash8422 <120177211+yash8422@users.noreply.github.com>
2025-06-20 05:39:38 +0530

    Update slash.py

--- slash.py
+++ slash.py
@@ -1,4 +1,6 @@
-import streamlit as st import pandas as pd import plotly.express as px
+import streamlit as st 
+import pandas as pd 
+import plotly.express as px
 
 st.set_page_config(page_title="Slash Dynamics", layout="wide")
 
